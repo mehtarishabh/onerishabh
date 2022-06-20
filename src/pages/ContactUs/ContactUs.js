de489e3deb2af157461efd1151822a1e5557411b
Rishabh Mehta <rishabh.mehta@ymedialabs.com>
2022-06-20 19:42:33 +0530

    small fixes

--- src/pages/ContactUs/ContactUs.js
+++ src/pages/ContactUs/ContactUs.js
@@ -5,7 +5,7 @@ function ContactUs() {
     <div className='ContactUs'>
       <img className='About_image' alt="rishabh" src={window.location.origin + '/images/rishabh.jpg'}></img>
       <div className='ContactUs_text'>
-        <b>Phone:</b> 8747824255
+        <b>Phone:</b> +918747824255
       </div>
       <div className='ContactUs_text'>
         <b>Email:</b> mehta.rishabh0@gmail.com
