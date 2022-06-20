58edede5f1398ee6aceed362b8901d98633316a0
Rishabh Mehta <rishabh.mehta@ymedialabs.com>
2022-06-20 09:41:14 +0530

    mobile responsiveness added

--- src/ui/Footer/Footer.js
+++ src/ui/Footer/Footer.js
@@ -3,10 +3,11 @@ import './Footer.scss';
 function Footer() {
   return (
     <div className='Footer container'>
-      <div>© Copyright 2022. All right reserved</div>
-      <div>
+      <div className='Footer_copyright'>© Copyright 2022. All right reserved</div>
+      <div className='Footer_iconsContainer'>
         <a className='Footer_icons' href="https://www.linkedin.com/in/rishabh-mehta-76447b5a/" target="_blank" rel="noreferrer">LinkedIn</a>
         <a className='Footer_icons' href="https://github.com/mehtarishabh" target="_blank" rel="noreferrer">Github</a>
+        <a className='Footer_icons' href="https://codepen.io/rishabh247" target="_blank" rel="noreferrer">Codepen</a>
         <a className='Footer_icons' href="https://leetcode.com/user8099ea/" target="_blank" rel="noreferrer">Leetcode</a>
       </div>
     </div>
