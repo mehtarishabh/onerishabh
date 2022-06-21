9b3e8863054c590f7f56e677d12182e12b362a1a
Rishabh Mehta <rishabh.mehta@ymedialabs.com>
2022-06-22 02:25:03 +0530

    small fixes

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -10,6 +10,9 @@ function About() {
         I did my schooling from here and completed graduation from NIT Goa, which is one of the top tier college in india. I was 
         so fond of gadgets and android applications that it encouraged me to pursue career in computer science.
         <br /><br />
+        I am a senior programmer with good knowledge of front-end techniques. I love structure and order and I also stand for quality. 
+        I love spending time on fixing little details and optimizing web apps.
+        <br /><br />
         In my free time I love to watch sci fi movies like interstellar, contact, edge of tomorrow, gravity, martian etc. I am also a fan 
         of marvel movies and series. I also love to read novels based on real personalities. Among them my favourite one 
         is on Elon musk.
