52872004455774631cb2bb5e31d3b77104b3e78d
Rishabh Mehta <rishabh.mehta@ymedialabs.com>
2022-06-24 20:54:28 +0530

    added lazy loading and animation

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,14 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from '../pages/Home/Home'
-import About from '../pages/About/About'
-import ContactUs from '../pages/ContactUs/ContactUs'
 import Footer from '../ui/Footer/Footer'
 import './App.scss';
 import MainMenu from "../ui/MainMenu/MainMenu";
+
+const Home = lazy(() => import('../pages/Home/Home'));
+const About = lazy(() => import('../pages/About/About'));
+const ContactUs = lazy(() => import('../pages/ContactUs/ContactUs'));
+const renderLoader = () => <p>Loading</p>;
 
 function App() {
   const [showMainMenu, setShowMainMenu] = useState(false);
@@ -31,20 +33,22 @@ function App() {
 
   return (
     <BrowserRouter>
-      {!showMainMenu && <div className='App_mainMenu' onClick={() => setShowMainMenu(true)}>
-        <div className='App_mainMenu_top'></div>
-        <div className='App_mainMenu_bottom'></div>
-      </div>}
-      {showMainMenu && <div ref={wrapperRef}>
-        <MainMenu closeClick={() => setShowMainMenu(false)}/>
-      </div>}
-      <Routes>
-        <Route exact path="/" element={<Home />}></Route>
-        <Route exact path="/about" element={<About />}></Route>
-        <Route exact path="/contact" element={<ContactUs />}></Route>
-        <Route render={() => <h1>Not found</h1>}></Route>
-      </Routes>
-      <Footer />
+      <Suspense fallback={renderLoader()}>
+        {!showMainMenu && <div className='App_mainMenu' onClick={() => setShowMainMenu(true)}>
+          <div className='App_mainMenuIcon_top'></div>
+          <div className='App_mainMenuIcon_bottom'></div>
+        </div>}
+        {showMainMenu && <div ref={wrapperRef}>
+          <MainMenu closeClick={() => setShowMainMenu(false)}/>
+        </div>}
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/about" element={<About />}></Route>
+          <Route exact path="/contact" element={<ContactUs />}></Route>
+          <Route render={() => <h1>Not found</h1>}></Route>
+        </Routes>
+        <Footer />
+      </Suspense>
     </BrowserRouter>
   );
 }
