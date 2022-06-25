3c8e6b475dfb715797586aeb8716c82aeb8a4c71
Rishabh Mehta <rishabh.mehta@ymedialabs.com>
2022-06-25 08:47:11 +0530

    menu animation added

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, lazy, Suspense } from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Footer from '../ui/Footer/Footer'
@@ -11,45 +11,21 @@ const ContactUs = lazy(() => import('../pages/ContactUs/ContactUs'));
 const renderLoader = () => <p>Loading</p>;
 
 function App() {
-  const [showMainMenu, setShowMainMenu] = useState(false);
-  const wrapperRef = useRef(null);
-
-  useEffect(() => {
-    const checkIfClickedOutside = e => {
-      // If the menu is open and the clicked target is not within the menu,
-      // then close the menu
-      if (showMainMenu && wrapperRef.current && !wrapperRef.current.contains(e.target)) {
-        setShowMainMenu(false)
-      }
-    }
-
-    document.addEventListener("mousedown", checkIfClickedOutside)
-
-    return () => {
-      // Cleanup the event listener
-      document.removeEventListener("mousedown", checkIfClickedOutside)
-    }
-  }, [showMainMenu])
-
   return (
-    <BrowserRouter>
-      <Suspense fallback={renderLoader()}>
-        {!showMainMenu && <div className='App_mainMenu' onClick={() => setShowMainMenu(true)}>
-          <div className='App_mainMenuIcon_top'></div>
-          <div className='App_mainMenuIcon_bottom'></div>
-        </div>}
-        {showMainMenu && <div ref={wrapperRef}>
-          <MainMenu closeClick={() => setShowMainMenu(false)}/>
-        </div>}
-        <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route exact path="/about" element={<About />}></Route>
-          <Route exact path="/contact" element={<ContactUs />}></Route>
-          <Route render={() => <h1>Not found</h1>}></Route>
-        </Routes>
-        <Footer />
-      </Suspense>
-    </BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
+        <Suspense fallback={renderLoader()}>
+          <MainMenu />
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route exact path="/about" element={<About />}></Route>
+            <Route exact path="/contact" element={<ContactUs />}></Route>
+            <Route render={() => <h1>Not found</h1>}></Route>
+          </Routes>
+          <Footer />
+        </Suspense>
+      </BrowserRouter>
+    </div>
   );
 }
 
