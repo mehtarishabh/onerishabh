5c5ff9e4a7393d431d89732ec1fc452e8b530553
Rishabh Mehta <rishabh.mehta@ymedialabs.com>
2022-07-10 23:50:16 +0530

    small fixes

--- src/ui/MainMenu/MainMenu.test.js
+++ src/ui/MainMenu/MainMenu.test.js
@@ -17,23 +17,23 @@ const renderWithRouter = (component) => {
 
 it('should render the main menu with links after clicking menu', () => {
 
-  const { container, getByTestId } = renderWithRouter(<MainMenu />)
+  const { container } = renderWithRouter(<MainMenu />)
   
-  fireEvent.click(getByTestId('menu-link'))
+  fireEvent.click(screen.getByTestId('menu-link'))
 
-  const home_link = getByTestId('home-link');
+  const home_link = screen.getByTestId('home-link');
   expect(container).toContainElement(home_link);
 
-  const about_link = getByTestId('about-link');
+  const about_link = screen.getByTestId('about-link');
   expect(container).toContainElement(about_link);
 
-  const contact_link = getByTestId('contact-link');
+  const contact_link = screen.getByTestId('contact-link');
   expect(container).toContainElement(contact_link);
 })
 
 it('should not render the main menu with links before clicking menu', () => {
 
-  const { container, getByTestId } = renderWithRouter(<MainMenu />)
+  const { container } = renderWithRouter(<MainMenu />)
 
   const home_link = screen.queryByTestId('home-link');
   expect(container).not.toContainElement(home_link);
