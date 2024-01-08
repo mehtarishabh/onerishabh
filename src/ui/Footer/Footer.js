b2fd7c21026a0c44c21a5802f5f2487a54648846
Rishabh <rishabh@Rishabhs-MacBook-Air.local>
2024-01-08 17:22:33 +0530

    add certifications

--- src/ui/Footer/Footer.js
+++ src/ui/Footer/Footer.js
@@ -5,10 +5,10 @@ function Footer() {
     <div className='Footer container'>
       <div className='Footer_copyright'>© Copyright 2022. All right reserved</div>
       <div className='Footer_iconsContainer'>
-        <a className='Footer_icons' href="https://www.linkedin.com/in/rishabh-mehta-76447b5a/" target="_blank" rel="noreferrer">LinkedIn</a>
+        <a className='Footer_icons' href="https://www.linkedin.com/in/rishabh-mehta-nit/" target="_blank" rel="noreferrer">LinkedIn</a>
         <a className='Footer_icons' href="https://github.com/mehtarishabh" target="_blank" rel="noreferrer">Github</a>
         <a className='Footer_icons' href="https://codepen.io/rishabh247" target="_blank" rel="noreferrer">Codepen</a>
-        <a className='Footer_icons' href="https://leetcode.com/user8099ea/" target="_blank" rel="noreferrer">Leetcode</a>
+        <a className='Footer_icons' href="https://leetcode.com/mehta_rishabh/" target="_blank" rel="noreferrer">Leetcode</a>
       </div>
     </div>
   );
